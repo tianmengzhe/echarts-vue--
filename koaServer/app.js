e117bfd71004abb068e326ca3cc6b5cec5d6e51d
Chenlijun <leechen8@gmail.com>
2020-10-13 11:43:38 +0800

    计算服务器消耗时长 中间件

--- koaServer/app.js
+++ koaServer/app.js
@@ -1,9 +1,15 @@
 const Koa = require('koa')
-const app = new koa()
+// 创建实例
+const app = new Koa()
 
+// app.use((ctx, next)=>{
+//     ctx.response.body = {msg:'连接成功'}
+// })
 
-app.use((ctx, next)=>{
+const duration = require('./middleware/koa_response_duration')
 
-})
+// 使用中间件
+app.use(duration)
 
+// 监听端口
 app.listen(3000,()=>console.log('服务器启动成功 http://localhost:3000'))
--- koaServer/middleware/koa_response_duration.js
+++ koaServer/middleware/koa_response_duration.js
@@ -1,3 +1,20 @@
 /**
  * 计算服务器消耗时长
+ * 
+ * 位于第一层中间件
+ * 
+ * 开始记录时间 其它中间件执行完成记录时间
+ * 
+ * 设置响应头 响应时间 X-Response-Time: 5ms
  * */ 
+
+module.exports = async (ctx, next)=>{
+    const start = Date.now()
+    // 执行中间件
+    await next()
+    const end = Date.now()
+    const duration = end - start
+
+    // 设置响应头 ctx.set
+    ctx.set('X-Response-Time', duration+'ms')
+}
